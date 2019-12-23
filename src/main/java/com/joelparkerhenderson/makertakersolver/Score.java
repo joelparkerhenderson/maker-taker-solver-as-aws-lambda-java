c4c11dcdbe5ff1d90326f946653e0d1b964c3299
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-12-22 21:23:52 -0800

    Add score math

--- src/main/java/com/joelparkerhenderson/makertakersolver/Score.java
+++ src/main/java/com/joelparkerhenderson/makertakersolver/Score.java
@@ -6,6 +6,7 @@ public class Score {
     int soft;
 
     static final Score ZERO = new Score(0,0);
+    static final Score ONE = new Score(1,1);
 
     public int getHard() {
         return hard;
@@ -21,6 +22,30 @@ public class Score {
 
     public void setSoft(final int soft) {
         this.soft = soft;
+    }
+
+    public void add(Score score) {
+        if (score == Score.ZERO) return;
+        this.hard += score.hard;
+        this.soft += score.soft;
+    }
+
+    public void subtract(Score score) {
+        if (score == Score.ZERO) return;
+        this.hard -= score.hard;
+        this.soft -= score.soft;
+    }
+
+    public void multiply(Score score) {
+        if (score == Score.ONE) return;
+        this.hard *= score.hard;
+        this.soft *= score.soft;
+    }
+
+    public void divide(Score score) {
+        if (score == Score.ONE) return;
+        this.hard /= score.hard;
+        this.soft /= score.soft;
     }
 
 	public Score(final int hard, final int soft) {
